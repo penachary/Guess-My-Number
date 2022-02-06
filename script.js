a451a44525d3cfd0d3868e9ec05f06870f103fa9
penachary <95870603+penachary@users.noreply.github.com>
2022-02-06 03:57:38 +0300

    final updates
    adding all js code to the game

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 'use strict';
+let secretNumber = Math.trunc(Math.random()*20) + 1;
 
+let score = 20;
+let highscore = 0;
 document.querySelector(".check").addEventListener('click', function (){
-    const guess = Number(document.querySelector(".guess").value);
+const guess = Number(document.querySelector(".guess").value);
 
+const displayMessage = function(message) {
+    document.querySelector(".message").textContent = message;
+}
+
+        // when no input
     if(!guess){
-        document.querySelector(".message").textContent = "Wrong guess!!!"
+        displayMessage("NO Number!!!");
+        // when correct answer
+    } else if (guess === secretNumber) {
+        displayMessage("Correct Answer!!!");
+        document.querySelector("body").style.backgroundColor = "#30b347"; // background color green 
+        document.querySelector(".number").style.width = "30rem"; 
+        document.querySelector(".number").textContent = secretNumber;
+        
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
+    } else if (guess !== secretNumber) {
+        if(score > 1){
+            displayMessage(guess > secretNumber ? "Too High!" : "Too Low!"); 
+            score--;
+            document.querySelector(".score").textContent = score;
+        }else {
+            displayMessage("You lost the Game!");
+            document.querySelector(".score").textContent = 0;
+        }
     }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    score = 20;
+    document.querySelector(".guess").value = "";
+    document.querySelector(".score").textContent = score;
+    displayMessage("Start guessing...");
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".number").textContent = "?"
 });
